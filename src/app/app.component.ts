b802409e9361281ff4f2949238228bb55cd168f0
A. A. Sumitro <hello@aasumitro.id>
2022-08-15 17:11:48 +0800

    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavController, Platform} from '@ionic/angular';
+import {Deeplinks} from '@awesome-cordova-plugins/deeplinks/ngx';
+import {HomePage} from './home/home.page';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(
+    protected platform: Platform,
+    protected navController: NavController,
+    protected deepLinks: Deeplinks
+  ) {}
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.deepLinks.route({
+        '/home': HomePage,
+        '/home/:id': HomePage,
+      }).subscribe(match => {
+        console.log(match);
+      }, nomatch => {
+        console.log(nomatch);
+      });
+    });
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      console.log(params.get('id'));
+    });
+  }
 }
